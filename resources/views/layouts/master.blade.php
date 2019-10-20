c5996cd0cd6f947e9c1da2821a8999921daa6477
WerVa <werwa999@wp.pl>
2019-10-20 14:21:31 +0200

    add_layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,70 +15,173 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="#">
+
 </head>
+
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+<div class="outer">
 
-                </ul>
+    <div class="navigation">
+        <a href="#">Menu</a>
+    </div>
+    <div class="sidebar">
+        <!--LOGO-->
+        <div class="logo">
+            <a href="#"><img src="img/logogf.png" alt="gamefly.pl"></a>
+        </div>
+        <!--szukajka-->
+        <div class="sidebar-search">
+            <form>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Szukaj gry">
+                    <span class="input-group-btn">
+								 <button type="submit"><i class="fa fa-search"></i></button>
+							</span>
+                </div>
+            </form>
+        </div>
+        <!-- nawigacja -->
+        <div class="menu">
+            <li class="item" id="messages">
+                <a href="#" class="btn"></i>Odkrywaj</a>
+            </li>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('Front::login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('Front::register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('Front::register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+            <li class="item" id="settings">
+                <a href="#settings" class="btn"></i>Kategorie</a>
+                <div class="smenu">
+                    <a href="">Akcji</a>
+                    <a href="">Wyścigi</a>
+                </div>
+            </li>
+        </div>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('Front::logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+        <!-- sekcja logowania -->
+        <div class="login">
+        </div>
+    </div>
 
-                                <form id="logout-form" action="{{ route('Front::logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
+
+    <!-- sekcja główna -->
+
+    <div class="main">
+
+        <div id="carousel-example-generic" class="carousel slide carousel-fade">
+            <div class="carousel-inner">
+                <div class="item active">
+                </div>
+
+                <!-- TOP -->
+                <div class="service">
+                    <h3 class='strikearound'>TOP</h3>
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-xs-6">
+                            <!-- Service item -->
+                            <div class="service-item animated">
+                                <i class="icon-camera skyblue"></i>
+                                <!-- Service item heading -->
+                                <h4><a href="#">Neque perspiciatis</a></h4>
+                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia mollit anim id ests.</p>
                             </div>
-                        </li>
-                    @endguest
-                </ul>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-6">
+                            <div class="service-item animated">
+                                <i class="icon-envelope nblue"></i>
+                                <h4><a href="#">Finibus perspiciatis</a></h4>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium totam remo.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-6">
+                            <div class="service-item animated">
+                                <i class="icon-download blue"></i>
+                                <h4><a href="#">Cicero perspiciatis</a></h4>
+                                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself pain.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-6">
+                            <div class="service-item animated">
+                                <i class="icon-gamepad green"></i>
+                                <h4><a href="#">Malorum perspiciatis</a></h4>
+                                <p>At vero eos et accusamus et iusto odio dignissimos qui blanditiis praesentium itate none.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
+        <!-- Koniec topu -->
+        <!-- NEWS -->
+        <div class="service">
+            <h3 class='strikearound'>NEWS</h3>
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Service item -->
+                    <div class="service-item animated">
+                        <i class="icon-camera skyblue"></i>
+                        <!-- Service item heading -->
+                        <h4><a href="#">Neque perspiciatis</a></h4>
+                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia mollit anim id ests.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <div class="service-item animated">
+                        <i class="icon-envelope nblue"></i>
+                        <h4><a href="#">Finibus perspiciatis</a></h4>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium totam remo.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <div class="service-item animated">
+                        <i class="icon-download blue"></i>
+                        <h4><a href="#">Cicero perspiciatis</a></h4>
+                        <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself pain.</p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <div class="service-item animated">
+                        <i class="icon-gamepad green"></i>
+                        <h4><a href="#">Malorum perspiciatis</a></h4>
+                        <p>At vero eos et accusamus et iusto odio dignissimos qui blanditiis praesentium itate none.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Koniec NEEWS -->
 
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
+            <!-- Stopka -->
+            <footer>
+                <div class="row">
+                    <div class="col-md-12">
+                        <hr />
+                        <br />
+                        <p class="text-center">Copyright 2019</a></p>
+                        <br />
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <!-- Javascript files -->
+    <!-- jQuery -->
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.arbitrary-anchor.js') }}"></script>
+    <script src="{{ asset('js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/respond.min.js') }}"></script>
+    <script src="{{ asset('js/html5shiv.js') }}"></script>
+    <script src={{ asset('js/custom.js') }}""></script>
 </body>
 </html>
