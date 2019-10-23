d7be85f74e9c16bb281c48b1153f2251016b49f7
Krzysztof Wieczorek <krzysztof.wieczorek@sungroup.pl>
2019-10-23 23:44:51 +0200

    Unikatowe slugi - sprawdzanie na poziomie bazy

---
+++ database/migrations/2019_10_23_163502_make_slug_unique.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeSlugUnique extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('slug')->unique()->change();
+        });
+        Schema::table('games_categories', function (Blueprint $table) {
+            $table->string('slug')->unique()->change();
+        });
+        Schema::table('games_tags', function (Blueprint $table) {
+            $table->string('slug')->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->string('slug')->dropUnique();
+        });
+        Schema::create('games_categories', function (Blueprint $table) {
+            $table->string('slug')->dropUnique();
+        });
+        Schema::create('games_tags', function (Blueprint $table) {
+            $table->string('slug')->dropUnique();
+        });
+    }
+}
